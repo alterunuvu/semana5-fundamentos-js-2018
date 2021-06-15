349d3f357024869876bbb1c5faa2ef7e053a0d12
alteruvu <johan.quijano@alteru.co>
2021-06-14 21:40:22 -0500

    Función mostrarEdad()

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var nombre = "Violeta",
+  edad = 5;
+
+console.log(`El nombre de mi hija es ${nombre}`);
+
+function mostrarEdad() {
+  console.log(`El nombre de mi hija es ${nombre} y tiene ${edad} años`);
+}
+
+mostrarEdad();
